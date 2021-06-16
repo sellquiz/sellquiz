755096dc1ba7831c30038b32c4159021e61ba156
Andreas Schwenk <andi.schwenk@googlemail.com>
2021-06-16 14:49:07 +0200

    sync

---
+++ gen-doc.py
@@ -0,0 +1,55 @@
+#******************************************************************************
+#* SELL - SIMPLE E-LEARNING LANGUAGE                                          *
+#*                                                                            *
+#* Copyright (c) 2019-2021 TH Koeln                                           *
+#* Author: Andreas Schwenk, contact@compiler-construction.com                 *
+#*                                                                            *
+#* Funded by: Digitale Hochschule NRW                                         *
+#* https://www.dh.nrw/kooperationen/hm4mint.nrw-31                            *
+#*                                                                            *
+#* GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007                         *
+#*                                                                            *
+#* This library is licensed as described in LICENSE, which you should have    *
+#* received as part of this distribution.                                     *
+#*                                                                            *
+#* This software is distributed on "AS IS" basis, WITHOUT WARRENTY OF ANY     *
+#* KIND, either impressed or implied.                                         *
+#******************************************************************************/
+
+# This script generates a brief documentation
+
+src_path = "src/index.js"
+
+grammar = ''
+
+# grammar:
+tmp = ''
+f = open(src_path, "r")
+lines = f.readlines()
+f.close()
+for line in lines:
+    line = line.strip()
+    if line.startswith("//"):
+        tmp += line[3:] + "\n"
+    elif line.startswith("parse"):
+        grammar += tmp
+        tmp = ''
+    else:
+        tmp = ''
+
+# write output
+# doc = '''SELL - SIMPLE E-LEARNING LANGUAGE
+# Copyright (c) 2019-2021 TH Koeln
+# Author: Andreas Schwenk, contact@compiler-construction.com
+# LICENSE: GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+
+# >>> THIS FILE IS GENERATED AUTOMATICALLY <<<
+
+# ===== GRAMMAR =====
+
+# '''
+doc = grammar
+
+f = open("grammar.txt", "w")
+f.write(doc)
+f.close()
