701e4e015f4e85b1bced66b732d1decbd930df8f
Andreas Schwenk <andi.schwenk@googlemail.com>
2021-10-28 08:33:58 +0200

    sync

--- scripts/lecturegen/lecturegen.py
+++ scripts/lecturegen/lecturegen.py
@@ -17,36 +17,61 @@ subsubsec = 1
 
 sellquiz = ""
 parsing_sellquiz = False
+language = "en"
+
+class Quiz:
+    def __init__(self):
+        self.id = 0
+        self.src = ""
+
+quizzes = []
 
 for i, x in enumerate(input):
+    x = x[:-1]  # remove "\n"
     if i == 0:
         title = x
         continue
     if parsing_sellquiz and not x.startswith(">>>"):
-        sellquiz += x
+        sellquiz += x + "\n"
         continue
-    if x.startswith("###"):
-        content += "<h3>" + str(sec) + "." + str(subsec) + "." + str(subsubsec) + ". " + x[3:].strip() + "</h3>"
+    if x.startswith("%LANG="):
+        language = x[6:].lower()
+    elif x.startswith("###"):
+        content += "<h3>" + str(sec) + "." + str(subsec) + "." + str(subsubsec) + ". " + x[3:].strip() + "</h3>\n"
         subsubsec += 1
     elif x.startswith("##"):
-        content += "<h2>" + str(sec) + "." + str(subsec) + ". " + x[2:].strip() + "</h3>"
+        content += "<h2>" + str(sec) + "." + str(subsec) + ". " + x[2:].strip() + "</h2>\n"
         subsec += 1
         subsubsec = 1
     elif x.startswith("#"):
-        content += "<h1>" + str(sec) + ". " + x[1:].strip() + "</h3>"
+        content += "<h1>" + str(sec) + ". " + x[1:].strip() + "</h1>\n"
         sec += 1
         subsec = 1
         subsubsec = 1
     elif x.startswith(">>>"):
         parsing_sellquiz = not parsing_sellquiz
         if parsing_sellquiz == False:
-            print(sellquiz)
+            quiz = Quiz()
+            quiz.id = len(quizzes)
+            quiz.src = sellquiz
+            sellquiz = ""
+            quizzes.append(quiz)
+            content += "<div id=\"quiz-" + str(quiz.id) + "\"></div>\n"
     else:
-        content += x # + "<br/>"
+        content += "<p>" + x.replace("$","`") + "</p>\n"
+
+quizzes_output = "let quizzes_src=[\n"
+for i, quiz in enumerate(quizzes):
+    if i > 0:
+        quizzes_output += ",\n"
+    quizzes_output += "`" + quiz.src + "`\n"
+quizzes_output += "];"
 
 output = template.replace("$TITLE$", title)
+output = output.replace("$LANGUAGE$", language)
 output = output.replace("$LEADTEXT$", leadtext)
 output = output.replace("$CONTENT$", content)
+output = output.replace("$QUIZZES$", quizzes_output)
 
 f = open("out.html", "w")
 f.write(output)
