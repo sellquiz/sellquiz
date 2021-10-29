06eb70c32d56fcf7fe6262d84d4564ef19337d71
Andreas Schwenk <andi.schwenk@googlemail.com>
2021-10-29 16:19:26 +0200

    sync

--- authoring/install.sh
+++ authoring/install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-cd .. && npm install
-ln -s ../../node_modules/ .
-ln -s ../../build .
+cd ..
+npm install
+./build.sh
+cd authoring
+#ln -s ../../node_modules/ .
+#ln -s ../../build .
+rm -rf node_modules
+rm -rf build
+cp -r ../node_modules node_modules
+cp -r ../build build
